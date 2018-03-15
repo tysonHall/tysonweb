7710e24f68c76b94a56bebac593749c3e5392c3b
tysonHall <373298601@qq.com>
2018-03-15 17:10:37 +0800

    Revert "Revert "Initial commit""
    This reverts commit c8346900645ff9546b6aaa21601c4afdc698f20a.

---
+++ application/index/controller/Index.php
@@ -0,0 +1,62 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+
+class Index extends Controller{
+
+	private $messageDao = null;
+	function _initialize()
+	{
+		$this->messageDao = new \app\index\model\Message();
+	}
+    public function index()
+    {
+    	$allmsg = $this->messageDao->select();
+
+    	$show_msg = null;
+    	if(!empty($allmsg))
+    	{
+    		$randindex = rand(1, count($allmsg));
+            $show_msg = $allmsg[$randindex-1];
+    	}
+
+        $show_text = '';
+    	if($show_msg == null)
+    	{
+    		$show_text = "还没有人留言哦，你将是第一个！";
+    	}
+
+    	if($show_text != '')
+    	{
+    		$this->assign('show_text', $show_text);
+    		return view('showtext');
+    	}
+    	$this->assign('show_msg', $show_msg);
+        $this->assign('count', count($allmsg));
+
+    	return view('index');
+    }
+
+    public function addmsg()
+    {
+        $mcontent = input('post.mcontent', '', 'strip_tags');
+        $maddby = input('post.maddby', '', 'strip_tags');
+        $bgimg = input('post.bgimg', '', 'strip_tags');
+
+        $add_data = array(
+            'mcontent' => $mcontent,
+            'addtime' => time(),
+            'maddby' => $maddby,
+            'bgimg' => $bgimg);
+
+        $r = $this->messageDao->insert($add_data);
+        if($r)
+        {
+            $this->success('发布成功');
+        }
+        else
+        {
+            $this->error('发布失败');
+        }
+    }
+}
